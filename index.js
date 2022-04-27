ac1189b6ae3737f2f303d9e63a64ed3ea9386d23
Kristofer <kroldan@ford.com>
2022-04-27 13:55:50 -0400

    Add form data and submit button functionality

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const inventoryDisplayScreen = document.querySelector(".inventory-display");
+const inventoryDisplayTable = document.querySelector("tbody");
+const submitButton = document.querySelector("#submit");
+const newItemForm = document.querySelector("form");
 
+newItemForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    
+    const itemInformation = {
+        itemName: formData.get("item-name"),
+        itemSellIn: formData.get("item-sell-in"),
+        itemQuality: formData.get("item-quality")
+    }
+
+    const newInventoryItem = document.createElement("tr");
+    newInventoryItem.innerHTML = `
+        <td class="item-category">${itemInformation.itemName}</td>
+        <td>${itemInformation.itemSellIn}</td>
+        <td>${itemInformation.itemQuality}</td>
+    `
+    inventoryDisplayTable.append(newInventoryItem);
+})
+
+function checkItemQuality(itemObject){
+    
+}
