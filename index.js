ddc11f9fb06e0cc382c811e13e05d6c919e711d9
Kristofer <kroldan@ford.com>
2022-04-28 18:12:32 -0400

    Add quality calculation function

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const submitButton = document.querySelector("#submit");
 const newItemForm = document.querySelector("form");
 const nextButton = document.querySelector("#next");
 const previousButton = document.querySelector("#previous");
+const itemNameInput = document.querySelector("#item-name");
+const itemQualityInput = document.querySelector("#item-quality");
+
+itemNameInput.addEventListener("input", event => {
+	if (itemNameInput.value.toLowerCase().includes("sulfuras")) {
+		itemQualityInput.value = 80;
+		itemQualityInput.max = 80;
+		itemQualityInput.min = 80;
+	} else {
+		itemQualityInput.max = 50;
+		itemQualityInput.min = 0;
+	}
+})
 
 let itemInformation = {};
 let itemDisplayArray = [];
@@ -28,10 +41,6 @@ newItemForm.addEventListener("submit", (event) => {
     inventoryDisplayTable.append(newInventoryItem);
 })
 
-nextButton.addEventListener("click", () => {
-    subtractValues(); 
-})
-
 function getCategory(itemName) {
     if (itemName.includes("Aged Brie")) {
         return "aged"
@@ -41,8 +50,14 @@ function getCategory(itemName) {
         return "sulfuras"
     } else if (itemName.includes("Conjured")) {
         return "conjured"
-    }
+    } else 
+        return "none"
 }
+
+nextButton.addEventListener("click", () => {
+    nextDayQuality();
+    subtractValues(); 
+})
 
 function subtractValues() {
     itemDisplayArray.forEach(object => {
@@ -53,6 +68,50 @@ function subtractValues() {
         <td>${object.itemQuality}</td>
         `
         object.itemSellIn--;
+        inventoryDisplayTable.append(newInventoryItem);
+    })
+}
+
+function nextDayQuality () {
+    itemDisplayArray.forEach(object => {
+        if (object.itemCategory == "none") {
+            let calculatedQuality = object.itemQuality - 1
+            object.itemQuality--
+            return calculatedQuality
+        }
+        if (object.itemCategory == "aged") {
+            let calculatedQuality = object.itemQuality + 1
+            object.itemQuality++
+            return calculatedQuality
+        } 
+        if (object.itemCategory == "backstage") {
+            return "What the fuck"
+        }
+        if (object.itemCategory == "conjured") {
+            let calculatedQuality = object.itemQuality - 2
+            object.itemQuality--
+            object.itemQuality--
+            return calculatedQuality
+        }
+        if (object.itemCategory == "sulfuras") {
+            return object.itemQuality = 80;
+        }
+    })
+}
+
+previousButton.addEventListener("click", () => {
+    addValues();
+})
+
+function addValues() {
+    itemDisplayArray.forEach(object => {
+        const newInventoryItem = document.querySelector(".custom-inventory-item");
+        newInventoryItem.innerHTML = `
+        <td class="item-name">${object.itemName}</td>
+        <td class="item-sell-in">${(object.itemSellIn + 1)}</td>
+        <td>${object.itemQuality}</td>
+        `
+        object.itemSellIn++;
         inventoryDisplayTable.append(newInventoryItem);
     })
 }
